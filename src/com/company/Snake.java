3df493fbd0b819648b22a8851d3717bedac982a9
TonySu479 <tonysu479@gmail.com>
2021-04-28 19:41:40 +0200

    First Commit

---
+++ src/com/company/Snake.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Snake extends JFrame {
+    Snake() {
+        Board board = new Board();
+        add(board);
+        pack();
+        setTitle("Snake Game");
+        setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+}
